d22b873a64b71dbfbf3f0917360c439bd12fbf37
ygreksh <ygreksh@gmail.com>
2021-09-07 20:43:46 +0300

    4 только один пример упростил и изменил вывод

--- TaskManager2/Program.cs
+++ TaskManager2/Program.cs
@@ -10,7 +10,7 @@ namespace TaskManager2
         static void Main(string[] args)
         {
             // Create a scheduler that uses two threads.
-       LimitedTaskScheduler lcts = new LimitedTaskScheduler(2);
+       LimitedTaskScheduler lcts = new LimitedTaskScheduler(3);
        List<Task> tasks = new List<Task>();
 
        // Create a TaskFactory and pass it our custom scheduler.
@@ -18,45 +18,48 @@ namespace TaskManager2
        CancellationTokenSource cts = new CancellationTokenSource();
 
        // Use our factory to run a set of tasks.
-       Object lockObj = new Object();
-       int outputItem = 0;
+       Object locker = new Object();
 
-       for (int tCtr = 0; tCtr <= 2; tCtr++) 
+       for (int tCtr = 1; tCtr <= 5; tCtr++) 
        {
           int iteration = tCtr;
           Task t = factory.StartNew(() => 
           {
-             for (int i = 0; i < 3; i++) {
-                lock (lockObj) {
-                   Console.Write("{0} in task t-{1} on thread {2}; ",
+             /*
+             for (int i = 0; i < 2; i++)
+             {
+                lock (locker) {
+                   Console.WriteLine("{0} in task t-{1} on thread {2}; ",
                       i, iteration, Thread.CurrentThread.ManagedThreadId);
-                   outputItem++;
-                   if (outputItem % 3 == 0)
-                      Console.WriteLine();
+                   
                 }
              }
+             */
+             Console.WriteLine($"Задача {iteration} выполняется в потоке {Thread.CurrentThread.ManagedThreadId} ");
           }, cts.Token);
           tasks.Add(t);
       }
       // Use it to run a second set of tasks.
-      for (int tCtr = 0; tCtr <= 4; tCtr++) {
+      /*
+      for (int tCtr = 0; tCtr <= 4; tCtr++) 
+      {
          int iteration = tCtr;
          Task t1 = factory.StartNew(() => 
          {
-            for (int outer = 0; outer <= 3; outer++) {
-               for (int i = 0x21; i <= 0x25; i++) {
-                  lock (lockObj) {
-                     Console.Write("'{0}' in task t1-{1} on thread {2}   ",
+            for (int outer = 0; outer <= 3; outer++) 
+            {
+               for (int i = 0x21; i <= 0x25; i++) 
+               {
+                  lock (locker) 
+                  {
+                     Console.WriteLine("'{0}' in task t1-{1} on thread {2}   ",
                         Convert.ToChar(i), iteration, Thread.CurrentThread.ManagedThreadId);
-                     outputItem++;
-                     if (outputItem % 3 == 0)
-                        Console.WriteLine();
                   }
                }
             }
          }, cts.Token);
          tasks.Add(t1);
-      }
+      }*/
 
       // Wait for the tasks to complete before displaying a completion message.
       Task.WaitAll(tasks.ToArray());
