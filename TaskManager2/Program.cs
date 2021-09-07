cf169adb731f2ed0e33e8068431ec105cf1d6445
ygrek <ygreksh@gmail.com>
2021-09-07 18:03:32 +0300

    4 выровненные строки

--- TaskManager2/Program.cs
+++ TaskManager2/Program.cs
@@ -21,37 +21,40 @@ namespace TaskManager2
        Object lockObj = new Object();
        int outputItem = 0;
 
-       for (int tCtr = 0; tCtr <= 4; tCtr++) {
+       for (int tCtr = 0; tCtr <= 2; tCtr++) 
+       {
           int iteration = tCtr;
-          Task t = factory.StartNew(() => {
-                                       for (int i = 0; i < 3; i++) {
-                                          lock (lockObj) {
-                                             Console.Write("{0} in task t-{1} on thread {2}; ",
-                                                           i, iteration, Thread.CurrentThread.ManagedThreadId);
-                                             outputItem++;
-                                             if (outputItem % 3 == 0)
-                                                Console.WriteLine();
-                                          }
-                                       }
-                                    }, cts.Token);
+          Task t = factory.StartNew(() => 
+          {
+             for (int i = 0; i < 3; i++) {
+                lock (lockObj) {
+                   Console.Write("{0} in task t-{1} on thread {2}; ",
+                      i, iteration, Thread.CurrentThread.ManagedThreadId);
+                   outputItem++;
+                   if (outputItem % 3 == 0)
+                      Console.WriteLine();
+                }
+             }
+          }, cts.Token);
           tasks.Add(t);
       }
       // Use it to run a second set of tasks.
       for (int tCtr = 0; tCtr <= 4; tCtr++) {
          int iteration = tCtr;
-         Task t1 = factory.StartNew(() => {
-                                       for (int outer = 0; outer <= 3; outer++) {
-                                          for (int i = 0x21; i <= 0x25; i++) {
-                                             lock (lockObj) {
-                                                Console.Write("'{0}' in task t1-{1} on thread {2}   ",
-                                                              Convert.ToChar(i), iteration, Thread.CurrentThread.ManagedThreadId);
-                                                outputItem++;
-                                                if (outputItem % 3 == 0)
-                                                   Console.WriteLine();
-                                             }
-                                          }
-                                       }
-                                    }, cts.Token);
+         Task t1 = factory.StartNew(() => 
+         {
+            for (int outer = 0; outer <= 3; outer++) {
+               for (int i = 0x21; i <= 0x25; i++) {
+                  lock (lockObj) {
+                     Console.Write("'{0}' in task t1-{1} on thread {2}   ",
+                        Convert.ToChar(i), iteration, Thread.CurrentThread.ManagedThreadId);
+                     outputItem++;
+                     if (outputItem % 3 == 0)
+                        Console.WriteLine();
+                  }
+               }
+            }
+         }, cts.Token);
          tasks.Add(t1);
       }
 
